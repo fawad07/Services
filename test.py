84e7245fe783c93234b3c8f1d7daa53a108d9680
Muhammad Fawad Aleem <muhammad.f.aleem@gmail.com>
2024-06-15 19:51:40 -0700

    Update test.py
    mp3 downloads in the directory.
    Work on having the user download mp3 
    same as download bk_rm image

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
 
 
 from flask import Flask, render_template, request, send_file, redirect, url_for, jsonify
@@ -13,19 +10,10 @@ from pytube import YouTube
 import re
 
 
-# In[ ]:
-
-
 app = Flask(__name__)
 
 
-# In[ ]:
-
-
-#BACKGROUND REMOVER HELPER FUNC
-
-
-# In[ ]:
+# BACKGROUND REMOVER HELPER FUNC
 
 
 def check_image_loaded(image_path):
@@ -35,6 +23,7 @@ def check_image_loaded(image_path):
         print(f"Error: Unable to load image '{image_path}'.")
         return False
     return True
+
 
 def remove_background(image_path, output_path):
     # Read the input image
@@ -48,13 +37,7 @@ def remove_background(image_path, output_path):
     print(f"Background removed. Result saved as {output_path}")
 
 
-# In[ ]:
-
-
-#YOUTUBE 2 MP3 CONVERT HELPER FUNC
-
-
-# In[ ]:
+# YOUTUBE 2 MP3 CONVERT HELPER FUNC
 
 
 def download_progress(stream, chunk, bytes_remaining):
@@ -63,16 +46,19 @@ def download_progress(stream, chunk, bytes_remaining):
     percent = (bytes_downloaded / stream.filesize) * 100
     print(f"\r{percent:.1f}% downloaded", end="", flush=True)
 
+
 def download_youtube_audio(video_url):
     yt = YouTube(video_url)
+
     stream = yt.streams.filter(only_audio=True).first()
-    stream.download(output_path='.')
-    stream.register_on_progress_callback(download_progress)
-    print("\n")  # Add a newline after download completion
+    stream.download(output_path=".")
+    # stream.register_on_progress_callback(download_progress)
+    # print("\n")  # Add a newline after download completion
+
 
 def is_youtube_url(url):
     # Regular expression pattern for matching YouTube video URLs
-    youtube_pattern = r'^(https?://)?(www\.)?youtube\.com/'
+    youtube_pattern = r"^(https?://)?(www\.)?youtube\.com/"
     # Check if the URL matches the pattern
     if re.match(youtube_pattern, url):
         return True
@@ -80,35 +66,37 @@ def is_youtube_url(url):
         return False
 
 
-# In[ ]:
+# ROUTES
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/upload', methods=['POST'])
+@app.route("/upload", methods=["POST"])
 def upload():
-    if 'file' not in request.files:
-        return 'No file part'
-    file = request.files['file']
-    if file.filename == '':
-        return 'No selected file'
+    if "file" not in request.files:
+        return "No file part"
+    file = request.files["file"]
+    if file.filename == "":
+        return "No selected file"
     # Check if the file is a valid image
     filename = file.filename
-    allowed_extensions = {'png', 'jpg', 'jpeg', 'gif'}
-    if '.' not in filename or filename.split('.')[-1].lower() not in allowed_extensions:
-        return 'Invalid file format. Only PNG, JPG, JPEG, and GIF files are allowed.'
+    allowed_extensions = {"png", "jpg", "jpeg", "gif"}
+    if "." not in filename or filename.split(".")[-1].lower() not in allowed_extensions:
+        return "Invalid file format. Only PNG, JPG, JPEG, and GIF files are allowed."
     # Save the uploaded image to a temporary location
-    upload_folder = 'uploads'
+    upload_folder = "uploads"
     os.makedirs(upload_folder, exist_ok=True)
     upload_path = os.path.join(upload_folder, filename)
     file.save(upload_path)
     # Process the uploaded image to remove background
-    output_folder = 'background_removal_result'
+    output_folder = "background_removal_result"
     os.makedirs(output_folder, exist_ok=True)
-    output_path = os.path.join(output_folder, f"{os.path.splitext(filename)[0]}_background_removed.png")
+    output_path = os.path.join(
+        output_folder, f"{os.path.splitext(filename)[0]}_background_removed.png"
+    )
     # Check if the image was loaded successfully
     is_loaded = check_image_loaded(upload_path)
     if is_loaded:
@@ -117,42 +105,39 @@ def upload():
         # Return the background-removed image for download
         return send_file(output_path, as_attachment=True)
     else:
-        return 'Error processing uploaded image'
-    
+        return "Error processing uploaded image"
+
 
 @app.route("/convert", methods=["POST"])
 def convert():
     youtube_url = request.form.get("youtube_url")
+
     # Check if youtube_url is empty or not provided
     if not youtube_url:
         # return 'Error: YouTube URL is missing or empty'
         return jsonify({"error": "YouTube URL is missing or empty"}), 400
     if is_youtube_url(youtube_url):
+        print(f"Received YouTube URL: {youtube_url}")
+
+        # Process the YouTube URL, e.g., perform conversion to MP3
         download_youtube_audio(youtube_url)
         print("VALID")
+        return redirect(
+            url_for("home")
+        )  # "Download Started. Check console for progress"
     else:
+        return jsonify({"error": "Invalid YouTube URL"}), 400
         print("Invalad url")
 
-    # Process the YouTube URL, e.g., perform conversion to MP3
     # Add your logic here based on what you want to do with the URL
     # For now, let's just print it
-    print(f"Received YouTube URL: {youtube_url}")
+
     # Redirect back to the homepage or another page after processing
-    return "Under construction"  # redirect(url_for('index.html'))
+    #return "Under construction"		#redirect(url_for("home"))
 
 
-# In[ ]:
+if __name__ == "__main__":
+    # app.run()
+    app.run(debug=True, port=5000, use_reloader=False)  # Set port to 5000
 
-
-if __name__ =="__main__":
-    #app.run()
-    app.run(debug=True, port=5000, use_reloader=False) # Set port to 5000
-    
-    #https://www.youtube.com/watch?v=SF-Q9ucn9ko <-- Example url
-
-
-# In[ ]:
-
-
-
-
+    # https://www.youtube.com/watch?v=SF-Q9ucn9ko <-- Example url
